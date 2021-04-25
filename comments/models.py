71f59575c155edb7b9bf10cb195c6b37a649631e
rrxrs <kadoshrr2019@gmail.com>
2021-04-24 23:52:51 -0500

    Modified attributes names Model Comments

--- comments/models.py
+++ comments/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Comment(models.Model):
-    message = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=250)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.id
