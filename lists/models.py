8ecdb53b31fc46ca4bd9b3b909bf48c70fb1b808
rrxrs <kadoshrr2019@gmail.com>
2021-04-24 23:47:55 -0500

    Fixed attributes names Model lists

--- lists/models.py
+++ lists/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class List(models.Model):
-    title = models.CharField(max_length=255)
-    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=250)
+    creation_date = models.DateTimeField(auto_now_add=True)
     position = models.IntegerField(null=True)
 
     def __str__(self):
