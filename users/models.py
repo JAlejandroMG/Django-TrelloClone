94856e4b449a1135bc5089b95330f26b02c942a5
JAlejandroMG <jalejandromg@tutanota.com>
2021-04-28 22:50:30 -0500

    Fixed: CustomUser. Added: Hashed password, UUID

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
@@ -7,11 +8,14 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     username = None
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=50, blank=False)
+    last_name = models.CharField(_('last name'), max_length=50, blank=False)
     favorites = models.ManyToManyField('boards.Board', related_name='users', blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     objects = CustomUserManager()
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import CustomUser
-from users.serializers import UsersSerializer, UsersDetailSerializer
+from users.serializers import UsersSerializer, UsersDetailSerializer, CreateUserSerializer
 
 
 class UsersViewSet(ModelViewSet):
@@ -12,4 +12,6 @@ class UsersViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return UsersDetailSerializer
+        if self.request.method == 'POST':
+            return CreateUserSerializer
         return UsersSerializer
